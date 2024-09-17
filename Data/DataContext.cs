a56510a3e2595f39f40a876c694620b08775113d
Cristobal Pedroza <mcristobalolaf@gmail.com>
2024-09-17 01:24:18 -0600

    First try

---
+++ Data/DataContext.cs
@@ -0,0 +1,4 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+public class DataContext (DbContextOptions options) : DbContext(options)
---
+++ Entities/AppUser.cs
@@ -0,0 +1,8 @@
+namespace API.Entitites;
+
+public class appUser 
+{
+    public int Id { get; set;}
+    public required string UserName { get; set;}
+
+}
