03176fdb540c2b5112c9f08764c138816cedc1a3
Cristobal Pedroza <mcristobalolaf@gmail.com>
2024-09-17 16:21:54 -0600

    SQlite first users

--- obj/Debug/net8.0/API.AssemblyInfo.cs
+++ obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+237a6a45b6ec55ad277f132ab8f077fcb0e4be7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+692eef3354009acdd86a393a7ae7c7df52ff114b")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
