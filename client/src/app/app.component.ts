ba15e6891211f9199c034a62e5d6458d1d1d3f46
Cristobal Pedroza <mcristobalolaf@gmail.com>
2024-09-17 21:29:06 -0600

    Add cors

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, Inject, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,6 +9,17 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  http= inject(HttpClient);
+  title = 'Xamira';
+  users: any;
+
+  ngOnInit(): void {
+    this.http.get("http://localhost:5162/api/v1/users").subscribe({
+      next: (response) => { this.users = response },
+      error: (error) => { console.log(error) },
+      complete: () => { console.log("Requested completed") }
+    });
+  }
+
 }
--- obj/Debug/net8.0/API.AssemblyInfo.cs
+++ obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53bbd13e8d8958f1cca4c2f663094809720af8ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+337825bcb626b8572d2f2614d4efcedca332fe7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
