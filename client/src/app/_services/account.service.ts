d98d1a3aaea892e14dcd76a0ae91625ee9a36e93
Cristobal Pedroza <mcristobalolaf@gmail.com>
2024-10-26 12:06:41 -0600

    Parcial03 - Add members support

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -2,17 +2,18 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
 import { map, Observable } from 'rxjs';
 import { User } from '../_models/user';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
   private http = inject(HttpClient);
-  baseurl = "https://localhost:5001/api/"
+  baseUrl = environment.apiUrl;
   currentUser = signal<User | null>(null);
   
   login(model : any):  Observable<User | void>{
-    return this.http.post<User>(this.baseurl + "account/login",model).pipe(
+    return this.http.post<User>(this.baseUrl + "account/login",model).pipe(
       map((user) =>{
         if (user){
           localStorage.setItem("user", JSON.stringify(user));
@@ -23,7 +24,7 @@ export class AccountService {
   }
 
   register(model : any):  Observable<User | void>{
-    return this.http.post<User>(this.baseurl + "account/register",model).pipe(
+    return this.http.post<User>(this.baseUrl + "account/register",model).pipe(
       map((user) =>{
         if (user){
           localStorage.setItem("user", JSON.stringify(user));
