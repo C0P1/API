310537ff6dd237ea0b261ab5fc0a0088848454cb
Cristobal Pedroza <mcristobalolaf@gmail.com>
2025-02-09 00:02:40 -0600

    Parcial04 - add photos

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -14,6 +14,7 @@ public static class ApplicationServiceExtensions
         services.AddCors();
         services.AddScoped<ITokenServices, TokenServices>();
         services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IPhotoService,PhotoService>();
         services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
         services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
         return services;
